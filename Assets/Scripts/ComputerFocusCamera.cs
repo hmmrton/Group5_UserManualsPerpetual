0daff7b1c40f0b6adafb581b758cf45f1ecf26ed
Alex Wissing <awissing123@gmail.com>
2021-11-19 15:27:13 -0600

    desktop icons... that'll be interesting (im thinking hotkeys, ie press 1 to open game 1, press 2 to open game 2) for now, otherwise i gotta go into the mathematical weeds. Raw images are non-interactive meaning that events aren't triggered through a raw image. So if we load a scene in the raw image, clicking aint available (unless i do some wacky math). I think most of our mini games dont need clicking but we ought to double check. I did add the zoom. Hit left control and see a p decent lerped zoom feature

---
+++ Assets/Scripts/ComputerFocusCamera.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerFocusCamera : MonoBehaviour
+{
+    // Unzoomed:
+    // position x -> 0 y -> 2 z -> 30
+    float unzoomedPosY = 2.0f;
+    float unzoomedPosZ = 30.0f;
+    // Zoomed:
+    // position x -> 0 y -> 1.75 z -> 31
+    float zoomedPosY = 1.75f;
+    float zoomedPosZ = 31.0f;
+
+    bool zoomed;
+    bool zooming;
+    bool zoomEnabled;
+    float nextZoom = 0.0f;
+    public float duration = 1.0f;
+
+    private void Start()
+    {
+        zoomed = false;
+        zooming = false;
+        zoomEnabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (zooming)
+        {
+            Debug.Log(zoomed ? "unfocusing" : "focusing");
+            Debug.Log("zoomEnabled? " + zoomEnabled);
+            Debug.Log("updates: " + nextZoom);
+            ZoomCamera();
+        }
+        if ((zoomEnabled && Input.GetAxis("FocusUnfocus") > 0))
+        {
+            Debug.Log("zoomin");
+            nextZoom = Time.time + duration;
+            zoomEnabled = false;
+            zooming = true;
+        }
+    }
+
+    void ZoomCamera()
+    {
+        Debug.Log("next " + nextZoom + " time time " + Time.time + " zoomin " + zooming);
+        if (nextZoom < Time.time)
+        {
+            StopZooming();
+            return;
+        }
+        if (zoomed)
+        {
+            gameObject.transform.position = Vector3.Lerp(
+                new Vector3(0, unzoomedPosY, unzoomedPosZ),
+                gameObject.transform.position,
+                Time.time / nextZoom
+            );
+        }
+        else
+        {
+            gameObject.transform.position = Vector3.Lerp(
+                new Vector3(0, zoomedPosY, zoomedPosZ),
+                gameObject.transform.position,
+                Time.time / nextZoom
+            );
+        }
+    }
+
+    void StopZooming()
+    {
+        zoomed = !zoomed;
+        gameObject.transform.position = zoomed ? new Vector3(0, zoomedPosY, zoomedPosZ) : new Vector3(0, unzoomedPosY, unzoomedPosZ);
+        zooming = false;
+        zoomEnabled = true;
+    }
+}
---
+++ Assets/Scripts/DesktopIcon.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class DesktopIcon : MonoBehaviour
+{
+    Button button;
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(handleOnClick);
+        Debug.Log("init buttonm");
+    }
+
+    void handleOnClick()
+    {
+        Debug.Log("loading " + sceneName);
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+}
