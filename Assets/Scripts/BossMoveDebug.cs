1dcccfdeaf93e389bdd04ad4193ef4f9f266458e
Alex Wissing <awissing123@gmail.com>
2021-12-04 19:33:33 -0600

    added ben's latest from google drive

--- Assets/Scripts/BossMoveDebug.cs
+++ Assets/Scripts/BossMoveDebug.cs
@@ -8,77 +8,157 @@ public class BossMoveDebug : MonoBehaviour
 
     private Vector3 target = new Vector3(10.79f, 2.79f, 57.15f);
     private Vector3 target1 = new Vector3(-7.79f, 2.79f, 57.15f);
+    private Vector3 target2 = new Vector3(-1.489339f, 2.79f, 57.15f);
     public float speed;
     private int currentPattern;
     public GameObject door;
     private int directionMove;
+    public GameObject bossLook;
+
+    //Random rand = new Random();
+    private int bossAIValue;
+
+    public AudioClip clip;
+    AudioSource audioSource;
+    
 
 
     void Start() 
     {
-        StartCoroutine(doorClose());
- 
+        bossLook.transform.localScale = new Vector3(0, 0, 0);
+        
         door.transform.eulerAngles = new Vector3(0, 0, 0);
         directionMove = 0;
+        //rand = new Random();
+        bossAIValue = 0;
+
+        
+
+        audioSource = GetComponent<AudioSource>();
+        
     }
     // Update is called once per frame
     void Update()
     {
+        if (bossDebugActive)
+            DebugCommand();
+        else
+            autoAction();
 
-        if (Input.GetKeyDown("1")) 
+       
+    }
+
+    void autoAction() 
+    {
+        if (currentPattern == 0) 
+        {
+            bossAIValue = Random.Range(1, 3);
+            currentPattern = bossAIValue;
+        }
+
+        if (currentPattern == 1 && transform.position == target1) 
+        {
+            StartCoroutine(doorClose());
+        }
+        DebugCommand();
+        if (transform.position == target1 || transform.position == target)
+            currentPattern = 0;
+    }
+
+    void DebugCommand() 
+    {
+        if (Input.GetKeyDown("1") && bossDebugActive)
         {
             currentPattern = 1;
             Debug.Log("Inititating Pattern One");
+            StartCoroutine(doorClose());
         }
 
-        if (bossDebugActive && currentPattern == 1 && directionMove == 0)
+        if (currentPattern == 1 && directionMove == 0)
         {
             transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
 
-            if (transform.position == target) 
+            if (transform.position == target)
             {
-                doorOpen();
+                //doorOpen();
                 //waiter();
-                doorClose();
+                //doorClose();
                 directionMove = 2;
             }
 
 
         }
 
-        if (bossDebugActive && currentPattern == 1 && directionMove == 1)
+        if (currentPattern == 1 && directionMove == 1)
         {
             transform.position = Vector3.MoveTowards(transform.position, target1, Time.deltaTime * speed);
 
             if (transform.position == target)
             {
                 doorOpen();
-                doorClose();
+
+                //doorClose();
                 directionMove = 0;
+
+
+                //gameObject.transform.eulerAngles = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             }
 
 
         }
+
+        if (Input.GetKeyDown("2") && bossDebugActive)
+        {
+            currentPattern = 2;
+            Debug.Log("Inititating Pattern Two");
+        }
+
+        if (currentPattern == 2)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target2, Time.deltaTime * speed);
+            if (transform.position == target2)
+            {
+                Debug.Log("Shrinking");
+                StartCoroutine(lookForward());
+                currentPattern = 1;
+                directionMove = 1;
+            }
+        }
+
+        if (transform.position == target1)
+        {
+            directionMove = 0;
+            currentPattern = 0;
+        }
+    }
+
+
+    IEnumerator lookForward() 
+    {
+        //yield return new WaitForSeconds(3);
+        transform.localScale = new Vector3(0, 0, 0);
+        bossLook.transform.localScale = new Vector3(3.58271f, 5.58494f, 4.4428f);
+
+        yield return new WaitForSecondsRealtime(2);
+        transform.localScale = new Vector3(3.58271f, 5.58494f, 4.4428f);
+        bossLook.transform.localScale = new Vector3(0, 0, 0);
     }
 
     void doorOpen() 
     {
         door.transform.eulerAngles = new Vector3(0, -50, 0);
         Debug.Log("Door Opened");
+        audioSource.Play();
     }
 
     IEnumerator doorClose() 
     {
-        yield return new WaitForSeconds(11);
+        yield return new WaitForSecondsRealtime(9);
+        doorOpen();
+        yield return new WaitForSecondsRealtime(2);
         door.transform.eulerAngles = new Vector3(0, 0, 0);
         Debug.Log("Door Closed");
-
-        if(directionMove == 2) 
-        {
-            directionMove = 1;
-            //gameObject.transform.eulerAngles = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        }
-            
+        
     }
 
     
