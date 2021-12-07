78ed747a89b25e150bcb30a78dc906c9ff6d10be
Alex Wissing <awissing123@gmail.com>
2021-12-06 19:16:08 -0600

    added loss menu that links back to main menu

--- Assets/Scripts/Controls/ControlsButtonHandler.cs
+++ Assets/Scripts/Controls/ControlsButtonHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ControlsButton : MonoBehaviour
+public class ControlsButtonHandler : MonoBehaviour
 {
     public void handleBackButtonClicked()
     {
