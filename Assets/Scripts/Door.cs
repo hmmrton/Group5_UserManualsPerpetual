6fe3e08fb8ee3d7ca95640acb5fcb8c2b0817691
Alex Wissing <awissing123@gmail.com>
2021-12-08 18:39:26 -0600

    added door script

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject door;
+    bool doorOpen;
+
+    Vector3 doorRotation; // rotation for the door to be open
+    // rotation for door being closed is zero vector
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorRotation = new Vector3(0, -50, 0);
+    }
+
+    public bool isDoorOpen()
+    {
+        return doorOpen;
+    }
+    public void openDoor()
+    {
+        if (!doorOpen)
+        {
+            door.transform.eulerAngles = doorRotation;
+            doorOpen = true;
+        }
+    }
+    public void closeDoor()
+    {
+        if (doorOpen)
+        {
+            door.transform.eulerAngles = Vector3.zero;
+            doorOpen = false;
+        }
+    }
+}
